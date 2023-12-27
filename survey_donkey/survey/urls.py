170e2acaac01524d28281413c0ae104c40f23a35
Zafer Arslan <zaferarslaan@gmail.com>
2023-12-27 18:26:32 +0300

    Add Answer Survey endpoint

--- survey_donkey/survey/urls.py
+++ survey_donkey/survey/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import SurveyList, SurveyDetail, UserSurveyList, UserRegistrationView, UserLoginView, TokenVerificationView
+from .views import SurveyList, SurveyDetail, UserSurveyList, UserRegistrationView, UserLoginView, TokenVerificationView, AnswerSurveyView, SendInvitations
 
 urlpatterns = [
     path('surveys/', SurveyList.as_view(), name='survey-list'),
     path('surveys/<int:pk>/', SurveyDetail.as_view(), name='survey-detail'),
+    path('surveys/<int:pk>/send-invitations/', SendInvitations.as_view(), name='send-invitations'),
     path('my-surveys/', UserSurveyList.as_view(), name='user-surveys'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('verify-token/<str:token>/', TokenVerificationView.as_view(), name='token-verification'),
+    path('answer-survey/<int:survey_id>/<str:token>/', AnswerSurveyView.as_view(), name='answer-survey'),
 ]
--- survey_donkey/survey/views.py
+++ survey_donkey/survey/views.py
@@ -10,8 +10,10 @@ from django.conf import settings
 from django.utils import timezone
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from django.shortcuts import get_object_or_404
 
-from .models import Survey, User
+
+from .models import Survey, User, Question, Answer, Invitation
 from .serializers import SurveySerializer
 
 User = get_user_model()
@@ -114,7 +116,7 @@ class TokenVerificationView(APIView):
         try:
             user = User.objects.get(login_token=token, login_token_expiration__gt=get_current_time())
             # Token is valid, so perform the required action (register or log in the user)
-            # You can create a session or a JWT token here for the user
+            # We can create a session or a JWT token here for the user
 
             # Optionally, clear the token after use
             user.login_token = None
@@ -127,3 +129,67 @@ class TokenVerificationView(APIView):
         
 def get_current_time():
     return timezone.now()
+        
+class AnswerSurveyView(APIView):
+    def get(self, request, survey_id, token):
+        try:
+            # Check if the invitation is valid
+            invitation = get_object_or_404(Invitation, token=token, survey_id=survey_id)
+            
+            if invitation.expiration_date < get_current_time():
+                return Response({"error": "Invitation expired"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # get the survey
+            survey = get_object_or_404(Survey, id=survey_id)
+            
+            survey_data = SurveySerializer(survey).data
+            
+            invitation.delete()
+            
+            return Response(survey_data, status=status.HTTP_200_OK)
+        except Survey.DoesNotExist:
+            return Response({"error": "Survey does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        except Invitation.DoesNotExist:
+            return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class SendInvitations(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, pk):
+        try:
+            survey = Survey.objects.get(id=pk, creator=request.user)
+        except Survey.DoesNotExist:
+            return Response({"error": "Survey does not exist or you are not the owner."}, status=status.HTTP_404_NOT_FOUND)
+        
+        emails = request.data.get('emails', [])
+        if not emails or not isinstance(emails, list):
+            return Response({"error": "Please provide a list of emails"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        for email in emails:
+            # Create an invitation for each email
+            invitation = Invitation.objects.create(survey=survey, email=email)
+            
+            # Construct the invitation URL
+            invitation_url = f"http://frontend-url.com/answer-survey/{survey.id}/{invitation.code}"
+            message = Mail(
+            from_email='surveydonkey@arslan.wtf',
+            to_emails=email,)
+
+            message.dynamic_template_data = {
+                'invitation_url': invitation_url
+            }
+            
+            message.template_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+            
+            try:
+                sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+                response = sg.send(message)
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+            except Exception as e:
+                print(str(e))
+                
+        return Response({"message": "Invitations sent"}, status=status.HTTP_200_OK)
+            
